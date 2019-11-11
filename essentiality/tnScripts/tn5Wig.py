cdcf61881ba5962f0b680424bc5acd85456592f7
Benjibendito <benjamin.j.perry@gmail.com>
2019-11-11 16:47:50 +1300

    Functional Tn5 wig generation.

--- essentiality/tnScripts/tn5Wig.py
+++ essentiality/tnScripts/tn5Wig.py
@@ -10,45 +10,84 @@
 # Python module for handling Tn5 related Tn-seq data.
 
 def tn5RefWig(fastafile):
-    '''(reference.fasta) -> tn5Wig[pos][count = 0]
+    """(reference.fasta) -> tn5Wig[pos][count = 0]
     :param fastafile: Input reference fasta file.
     :return: list[][] of [pos][count], with count = 0.
-    '''
-    pass
 
-def tn5WigUpdate(fastafile, bedfile, output):
-    pass
+    Reads in a single fasta sequence from a file.
+    Returns a wig file with every position at count set to 0.
+    Fasta header line is used to the wig config line.
+    """
 
-if __name__ == "__main__":
-    import argparse
-    import sys
+    print('\nComputing Reference Tn5 .wig file for: ' + fastafile)
+    inputFile = open(fastafile, 'r')
+    fastaData = inputFile.read()
 
-    parser = argparse.ArgumentParser()
+    # Check fastafile for a single header line.
+    if fastaData.count('>') > 1:
+        print('Error: More than one fasta header line in ' + fastafile)
+        raise SystemExit(1)
 
-    #Check for blank call, return usage
-    if len(sys.argv[1:]) == 0:
-        parser.print_usage()
-        parser.exit()
+    # Process the fasta file into a header and a sequence variables
+    fastaData = fastaData.splitlines()
+    headerLine = fastaData.pop(0)
+    headerLine = headerLine.split()[0]
+    headerLine = headerLine[1:]
+    fastaSequence = ''.join(fastaData)
 
-    ### Designate Inputs:
+    printHeader = ['variableStep chrom=' + str(headerLine)]
+    Wig = []
+    Wig.append(printHeader)
+    i = 1
 
-    # Input fastq file(s)
-    parser.add_argument("-f", "--fastqFile", type=str, help="path to input fastq file")
-    # Refence Genome Sequence
-    parser.add_argument("-r", "--refenceFile", type=str, help="path to reference file")
-    # Output File Name Base
-    parser.add_argument("-o", "--outputDir", type=str, help="output directory")
+    # Make a list containing each position in genome
+    for char in fastaSequence:
+        entry = [i, 0]
+        Wig.append(entry)
+        i += 1
 
-    # Tn trim string
-    parser.add_argument("-s", "--tnString", type=str, help="Tn substring to trim")
-
-    # Threads
-    parser.add_argument("-t", "--threads", type=str, help="processors to use")
-    # Memory
-    parser.add_argument("m", "--memory", type=str, help="Gb of memory to use")
+    return Wig
 
 
-    print('2018 Benjamin J Perry - (CC BY-NC-SA 4.0)')
-    print('Version: 2.1.0')
-    print('Email: benjamin.perry@postgrad.otago.ac.nz')
-    print('Citation: TBD\n')
+def tn5BedfileToTntags(bedfile):
+    """ ('path/to/bedfile.bed') -> tntagsList[]
+
+    Opens, reads, and parses a bedfile from tn5 tnseq given path to the file.
+    Returns a list, each element the genomic coordinate of tn-tag start offset by 9 bp.
+    This script takes into account the strand the read was mapped to.
+
+    """
+
+    # Open input bedfile
+    bedfileIn = open(bedfile, 'r')
+
+    # Process input bedfile
+    bedfileData = []
+    for line in bedfileIn:
+        items = line.rstrip('\r\n').split('\t')
+        items = [item.strip() for item in items]
+        bedfileData.append(items)
+
+    # Process tntag start positions
+    tntagsList = []
+    i = 0
+    errors = 0
+    for line in bedfileData:
+        if str(line[5]) == '+':
+            position = int(line[1])
+            position = position + 1  # offsets python counting from 0
+            position = position + 9  # offsets 9 bp duplication from Tn5
+            tntagsList.append(position)
+            i += 1
+        elif str(line[5]) == '-':
+            position = int(line[2])
+            position = position - 1  # offsets python counting from 0
+            position = position - 9  # offsets 9 bp duplication from Tn5
+            tntagsList.append(position)
+            i += 1
+        else:
+            errors += 1
+    print("Reads processed " + str(i))
+    print("Errors " + str(errors))
+
+    return tntagsList
--- essentiality/tnScripts/wigScripts.py
+++ essentiality/tnScripts/wigScripts.py
@@ -6,30 +6,31 @@
 # Status: Dev
 # Citation: TBD
 
-def bedfileToTntags(bedfile, tn5=False): #TODO Re-Write to Process as Readfile vs. Holding File in Memory
-    ''' ('path/to/bedfile.bed') -> tntagsList[]
+def bedfileToTntags(bedfile):
+
+    """ ('path/to/bedfile.bed') -> tntagsList[]
 
     Opens, reads, and parses a bedfile given path to the file.
     Returns a list, each element the genomic coordinate of tntag start.
     This script takes into account the strand the read was mapped to.
 
-    '''
+    """
 
-    #Open input bedfile
+    # Open input bedfile
     bedfileIn = open(bedfile, 'r')
 
-    #Process input bedfile
+    # Process input bedfile
     bedfileData = []
     for line in bedfileIn:
         items = line.rstrip('\r\n').split('\t')
         items = [item.strip() for item in items]
         bedfileData.append(items)
 
-    #Process tntag start positions
+    # Process tntag start positions
     tntagsList = []
     i = 0
     errors = 0
-    for line in bedfileData: #TODO update to check Tn5 Condition to implement the 9 bp offset
+    for line in bedfileData:  # TODO update to check Tn5 Condition to implement the 9 bp offset
         if str(line[5]) == '+':
             position = int(line[1])
             position = position + 1
@@ -47,29 +48,31 @@ def bedfileToTntags(bedfile, tn5=False): #TODO Re-Write to Process as Readfile v
 
     return tntagsList
 
+
 def updateWigList(tntagList, referenceWig):
-    '''(tntagList[], referenceWigList[][]) -> updatedWigList[][]
+    """(tntagList[], referenceWigList[][]) -> updatedWigList[][]
 
     Accepts a list of tn-tag positions and a reference .wig list. Sums unique tn-tag start positions and
     updates the counts for each start position in the reference wig list. Returns the updated wig list, with all
     unobserved tn-tag start positions set to 0, and all observed tn-tag start positions set to their counts.
 
-    '''
+    """
     import collections
 
     updatedWig = []
     # Make Counter for position list
     positionCounter = collections.Counter(tntagList)
     # Update count for each position in referenceWig
-    for siteTA in referenceWig:
+    for site in referenceWig:
         updatedEntry = []
-        updatedEntry.append(siteTA[0])
-        updatedEntry.append(positionCounter[siteTA[0]])
+        updatedEntry.append(site[0])
+        updatedEntry.append(positionCounter[site[0]])
         updatedWig.append(updatedEntry)
     return updatedWig
 
-def wigPipe(fastafile, bedfile, wigOutfile):
-    ''' ("path/fastafile", "path/bedfile", "path/wigOut") -> Null; Prints output wig file for treatment.
+
+def wigPipe(fastafile, bedfile, wigOutfile, Tn5=False):
+    """ ("path/fastafile", "path/bedfile", "path/wigOut", Tn5=False) -> Null; Prints output wig file for treatment.
 
     :param fastafile: input fasta file with a single >headerLine
     :param bedfile: tnseq data bedfile derived from read alignment
@@ -78,58 +81,87 @@ def wigPipe(fastafile, bedfile, wigOutfile):
     Takes a fasta file, and a corresponding bedfile of tntag alignments, and return a wig file of the tntag counts.
     Reference wig files can be generated using refGenTA.wigRefGen()
 
-    '''
+    """
     import refGenTA
     import wigScripts
+    import tn5Wig
+
     # Generate the reference wig counts list
-    print('Generating reference wig position list.')
-    referenceWig = refGenTA.wigRefGen(fastafile, printStatus=False)
-    printHeader = referenceWig.pop(0) #refGenTA must return print header as item[0]
-    # Transposon insertion counts from the bedfile
-    print('\nProcessing tntag insertions bedfile.')
-    tnCounts = wigScripts.bedfileToTntags(bedfile)
 
-    # Make Update tnCount wig track #TODO Add in Stranded .wig file output files
-    print('\nCreating treatment specific tntag wig track.')
-    treatmentWig = wigScripts.updateWigList(tnCounts, referenceWig)
+    if not Tn5:
+        print('Generating reference wig position list.')
+        referenceWig = refGenTA.wigRefGen(fastafile, printStatus=False)
+        printHeader = referenceWig.pop(0)  # refGenTA must return print header as item[0]
+        # Transposon insertion counts from the bedfile
+        print('\nProcessing tntag insertions bedfile.')
+        tnCounts = wigScripts.bedfileToTntags(bedfile)
 
-    wigPrint = open(wigOutfile, 'w')
-    print('\nWriting treatment wig file to: ' + str(wigPrint))
-    wigPrint.write(str(printHeader[0]) + '\n')
-    printReadsCount=0
-    for line in treatmentWig:
-        wigPrint.write(str(line[0]) + '\t' + str(line[1]) + '\n')
-        printReadsCount += line[1]
-    wigPrint.close()
-    print('\nTotal reads printed to .wig file: ' + str(printReadsCount))
+        # Make Update tnCount wig track
+        print('\nCreating treatment specific tntag wig track.')
+        treatmentWig = wigScripts.updateWigList(tnCounts, referenceWig)
 
-    raise SystemExit(0)
+        wigPrint = open(wigOutfile, 'w')
+        print('\nWriting treatment wig file to: ' + str(wigPrint))
+        wigPrint.write(str(printHeader[0]) + '\n')
+        printReadsCount = 0
+        for line in treatmentWig:
+            wigPrint.write(str(line[0]) + '\t' + str(line[1]) + '\n')
+            printReadsCount += line[1]
+        wigPrint.close()
+        print('\nTotal reads printed to .wig file: ' + str(printReadsCount))
+        raise SystemExit(0)
+
+    if Tn5:
+        print('Generating reference wig position list.')
+        referenceWig = tn5Wig.tn5RefWig(fastafile)
+        printHeader = referenceWig.pop(0)  # refWig must return print header as item[0]
+
+        # Transposon insertion counts from the bedfile
+        print('\nProcessing tntag insertions bedfile.')
+        tnCounts = tn5Wig.tn5BedfileToTntags(bedfile)
+
+        # Make Update tnCount wig track
+        print('\nCreating treatment specific tntag wig track.')
+        treatmentWig = wigScripts.updateWigList(tnCounts, referenceWig)
+
+        wigPrint = open(wigOutfile, 'w')
+        print('\nWriting treatment wig file to: ' + str(wigPrint))
+        wigPrint.write(str(printHeader[0]) + '\n')
+        printReadsCount = 0
+        for line in treatmentWig:
+            wigPrint.write(str(line[0]) + '\t' + str(line[1]) + '\n')
+            printReadsCount += line[1]
+        wigPrint.close()
+        print('\nTotal reads printed to .wig file: ' + str(printReadsCount))
+        raise SystemExit(0)
 
 if __name__ == '__main__':
     # Parse arguments
     import argparse
-    import refGenTA
     import sys
 
     parser = argparse.ArgumentParser()
 
     if len(sys.argv[1:]) == 0:
-        parser.print_usage() # for just the usage line
+        parser.print_usage()  # for just the usage line
         parser.exit()
 
-    parser.add_argument("-F", "--fastaFile", type = str, help="path to input .fasta file.")
-    parser.add_argument("-B", "--bedFile", type = str, help="path to input .bed file.")
-    parser.add_argument("-O", "--outputWigFile", type = str, help="path to output .wig file to be written.")
+    parser.add_argument("-F", "--fastaFile", type=str, help="path to input .fasta file.")
+    parser.add_argument("-B", "--bedFile", type=str, help="path to input .bed file.")
+    parser.add_argument("-O", "--outputWigFile", type=str, help="path to output .wig file to be written.")
+    parser.add_argument("-Tn5", "--Tn5Bedfile", action='store_true', help="Flag for Tn5 tnseq.")
     args = parser.parse_args()
 
     inFile = args.fastaFile
     outFile = args.outputWigFile
     bedFile = args.bedFile
+    Tn5 = args.Tn5Bedfile
+
 
     # Compute Wig Reference Track
-    wigPipe(inFile, bedFile, outFile)
+    wigPipe(inFile, bedFile, outFile, Tn5)
 
-    print("Treatment .wig file printed to: "+args.outputWigFile+"\n")
+    print("Treatment .wig file printed to: " + args.outputWigFile + "\n")
 
     print('2018 Benjamin J Perry - (CC BY-NC-SA 4.0)')
     print('Version: 2.1.0')
